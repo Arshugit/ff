0aabfc1822f35986ce143dfdb70d73aeb2bbf112
Arshugit <Arshuarshiya34@gmail.com>
2024-08-20 11:37:20 +0530

    Add files via upload

---
+++ qw.py
@@ -0,0 +1,31 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import matplotlib
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import matplotlib
+ModuleNotFoundError: No module named 'matplotlib'
+>>> 
+========================== RESTART: C:/Users/CSE/a.py ==========================
+Traceback (most recent call last):
+  File "C:/Users/CSE/a.py", line 1, in <module>
+    import matplotlib
+ModuleNotFoundError: No module named 'matplotlib'
+>>> 
+========================== RESTART: C:/Users/CSE/a.py ==========================
+Traceback (most recent call last):
+  File "C:/Users/CSE/a.py", line 1, in <module>
+    import matplotlib
+ModuleNotFoundError: No module named 'matplotlib'
+>>> 
+========================== RESTART: C:/Users/CSE/a.py ==========================
+Traceback (most recent call last):
+  File "C:/Users/CSE/a.py", line 1, in <module>
+    import numpy as np
+ModuleNotFoundError: No module named 'numpy'
+>>> 
+========================== RESTART: C:/Users/CSE/a.py ==========================
+Traceback (most recent call last):
+  File "C:/Users/CSE/a.py", line 1, in <module>
+    import matplotlib
+ModuleNotFoundError: No module named 'matplotlib'
